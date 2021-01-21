0b729ab27e47fe739753feca1293bfcd2d1662e8
marcinhodor <mhodor@o2.pl>
2021-01-21 22:25:34 +0100

    add Learn page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
-export default () => {
-  return <div>Hello world!</div>
+const Home = () => {
+  return (
+    <div style={{ color: `purple` }}>
+      <Header />
+      <Link to="/learn/">Learn ML</Link>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+    </div>
+  )
 }
+export default Home
---
+++ src/pages/learn.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+const Learn = () => {
+  return (
+    <div>
+      <Header />
+      <Link to="/">Home</Link>
+      <p>Learn some ML</p>
+    </div>
+  )
+}
+
+export default Learn
